00cbd6c59257000019b36f385c494e1a9939d08a
Lab 02 <lab02@ACADEMICO.LOCAL>
2026-03-09 19:18:20 -0300

    Primeiro Sprint

---
+++ script.js
@@ -0,0 +1,21 @@
+const btnSaibaMais = document.getElementById("btnSaibaMais");
+const btnEntrar = document.getElementById("btnEntrar");
+const btnCadastro = document.getElementById("btnCadastro");
+
+btnSaibaMais.addEventListener("click", function(){
+
+alert("Este sistema permite gerenciar patrocínios, eventos e resultados.");
+
+});
+
+btnEntrar.addEventListener("click", function(){
+
+alert("Redirecionar para tela de login.");
+
+});
+
+btnCadastro.addEventListener("click", function(){
+
+alert("Abrir cadastro de patrocínio.");
+
+});
