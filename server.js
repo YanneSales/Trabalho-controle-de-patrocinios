4db8ac06e63163d8ae52e8ba3bce2118970efddc
YS <yannesalesdev@gmail.com>
2026-05-25 19:30:23 -0300

    novo

--- server.js
+++ server.js
@@ -55,63 +55,6 @@ app.post('/login', (req, res) => {
     });
 });
 
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-// Configuração do Banco 
-const db = mysql.createPool({
-    host: 'k65y1l.h.filess.io', 
-    user: 'Controle_methodpen',
-    password: 'ea7d39c12221d686bbf724736cb0538c36e96618',
-    database: 'Controle_methodpen',
-    port: 3307, 
-    waitForConnections: true,
-    connectionLimit: 5, // Limite exato do Filess.io
-    queueLimit: 0
-});
-
-
-console.log("Pool de conexões configurado!");
-
-// Rota de Cadastro
-app.post('/cadastrar', (req, res) => {
-    const { nome, login, senha } = req.body; 
-    
-   
-    const sql = "INSERT INTO usuarios (nome, login, senha) VALUES (?, ?, ?)";
-    
-    db.query(sql, [nome, login, senha], (err, result) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ msg: "Erro no banco: " + err.message });
-        }
-        res.status(200).json({ msg: "Usuário gravado com sucesso!" });
-    });
-});
-
-// Rota de Login
-app.post('/login', (req, res) => {
-    const { login, senha } = req.body;
-    const sql = "SELECT * FROM usuarios WHERE login = ? AND senha = ?";
-
-    db.query(sql, [login, senha], (err, data) => {
-        if (err) {
-            return res.status(500).json({ msg: "Servidor acordando, tente novamente em alguns segundos " + err.message });
-        }
-        if (data.length > 0) {
-            res.status(200).json({ msg: "Login realizado!" });
-        } else {
-            res.status(401).json({ msg: "Credenciais inválidas" });
-        }
-    });
-});
-
 // ================= ROTAS DE PESSOAS / IDENTIFICAÇÃO =================
 
 // Verificar se pessoa já existe no sistema corporativo
@@ -188,7 +131,7 @@ app.patch('/solicitacao/avaliar/:id', (req, res) => {
     const { id } = req.params;
     const { status } = req.body; 
     
-    // Atualiza o status conforme regras do ENUM do Banco
+    // Atualiza o status conforme regras do ENUM de seu Banco
     const sql = "UPDATE solicitacao SET status = ? WHERE solicitacao_id = ?";
     db.query(sql, [status, id], (err, result) => {
         if (err) return res.status(500).json({ msg: err.message });
